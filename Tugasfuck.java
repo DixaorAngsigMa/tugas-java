07ed84dac6f48da39d580749bcbc544c18152e3e
Andika Rizz <AndikaRizkiFerdianto.TKJ1.24@binusasmg.sch.id>
2024-12-14 11:10:16 +0700

    Update tugasjava.java

--- Tugasfuck.java
+++ Tugasfuck.java
@@ -4,7 +4,7 @@ import java.util.Collections;
 import java.util.List;
 
 
-public class Tugasfuck {
+public class tugasjava {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
